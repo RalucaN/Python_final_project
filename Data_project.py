986720193ddd49161b6af47f04e94c1407388444
RalucaN <nicoarar@tcd.ie>
2019-12-05 12:46:29 +0000

    Update Data_project.py

--- Data_project.py
+++ Data_project.py
@@ -21,6 +21,7 @@ def read_data ():
 # Method 2
 data_panda = pd.read_csv("project_data.csv")
 
+''' 2
 # Step 2 - Exploring the data set
 
 def run ():
@@ -38,7 +39,9 @@ def run ():
     # Finding the type of data of each column
     print("The data type of each column: ")
     print(data_pd.dtypes)
+'''
 
+'''3
     # Missing data
     # a. Identifying the columns with no data
     print("Number of missing values per each column:")
@@ -46,12 +49,14 @@ def run ():
     print("Percentage of missing data in each column:")
     sum_miss_data = (data_pd.isnull().sum()) / len(data_pd)
     print(sum_miss_data)
-
+'''
+'''
     # b. Dropping columns with more than 50% missing data
     max_missing = 0.5
     data2 = data_pd.loc[:, (sum_miss_data <= max_missing)]
     print(data2.columns)
-
+'''
+'''
     # Extracting the authors
     data3 = data2.Authors.str.split(', ', expand=True).stack().str.strip().reset_index(level=1, drop=True)
     data3 = pd.DataFrame(data3, columns=["Author"])
@@ -59,6 +64,8 @@ def run ():
     data4 = data4.reset_index(drop=True)
     print(data4.loc[0:3, ])
 
+'''
+'''4
 # Step 3 - Descriptive statistics
     # a. Min, max, average and distribution of the data
 
@@ -66,16 +73,16 @@ def run ():
     print(data4["Year"].value_counts())
     print(data4["Author"].value_counts())
     print(data4["Publisher"].value_counts())
-
-  
+'''
+'''  
     # correlation between the different types of data
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
     correlations = data4.corr(method='pearson')
     print(correlations)
-    
-
+ '''  
+''' 5
      # b. Number of papers by year
     data5 = data4.dropna(subset=['Year'])
     data5.loc[:, "Year_int"] = data5["Year"].astype(int)
@@ -87,35 +94,30 @@ def run ():
     plt.ylabel("Number of Publications", fontsize=12)
     plt.xlabel('Year', fontsize=12)
     plt.show()
-
+'''
+'''
     # c. Age vs Cites
     x = data5["Age"]
     y = data5["Cites"]
-    plt.scatter(x, y, s=5, c='r')
+    plt.scatter(x, y, s=5, c='r', marker='x')
     plt.title('Age vs Cites')
     plt.xlabel('Age of the publication')
     plt.ylabel('Cites')
     plt.show()
-
- # Step 4 - Linear model
-    x = sm.add_constant(x)  # adding the constant
-    model = sm.OLS(y, x).fit()
-    predictions = model.predict(x)
+'''
+'''6
+ # Step 4 - Linear model and plot
+    x2 = sm.add_constant(x)  # adding the constant
+    model = sm.OLS(y, x2).fit()
+    predictions = model.predict(x2)
     print_model = model.summary()
     print(print_model)
-
-    #Plot with predicted values
-    plt.style.use('seaborn')
-    fix, ax = plt.subplots()
-    ax.scatter(data5['Age'], predictions, alpha=0.5,
-               label='predicted')
-    # Plot observed values
-    ax.scatter(data5['Age'], data5['Cites'], alpha=0.5,
-               label='observed')
-    ax.legend()
-    ax.set_title('OLS predicted values')
-    ax.set_xlabel('Age of the publication')
-    ax.set_ylabel('Cites')
+    sns.regplot(x, y, line_kws={"color": "r", "alpha": 0.7, "lw": 5})
+    plt.legend(loc="upper right")
+    plt.title('OLS predicted values')
+    plt.xlabel('Age of the publication')
+    plt.ylabel('Cites')
     plt.show()
+'''
 
 run()
